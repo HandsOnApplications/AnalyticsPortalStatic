edce80ae4365f33bd9fa7052bf578ddc7a784d92
pSirigala <psirigala@gmail.com>
2021-10-26 08:16:16 -0500

    few changes

--- api/reportsApi/index.js
+++ api/reportsApi/index.js
@@ -1,8 +1,8 @@
 
 module.exports = async function (context, req) {
-    const { Axios } = require("axios");
-    const getItemsEndpoint = "http://universities.hipolabs.com/search?country=United+States";
-    const response = await Axios.axios.get(getItemsEndpoint);
+    //const { Axios } = require("axios");
+    //const getItemsEndpoint = "http://universities.hipolabs.com/search?country=United+States";
+   // const response = await Axios.axios.get(getItemsEndpoint);
     //console.log(response);
     context.res.json(['phani', 'sirigala']);
 };
